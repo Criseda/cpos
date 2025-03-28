3b33e817aabe520cd03f7294106d007dc3933441
criseda <laurentiu.cristian.preda@gmail.com>
2025-03-28 20:37:34 +0000

    Add syscall interface #4

--- include/rust_interface.h
+++ include/rust_interface.h
@@ -22,4 +22,16 @@ void rust_init_heap(uintptr_t heap_start, size_t heap_size);
 void *rust_heap_alloc(size_t size);
 void rust_heap_free(void *ptr);
 
+// Syscall interface
+int32_t rust_syscall(uint32_t number, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+int32_t rust_handle_svc(uint32_t svc_number, uint32_t *args);
+
+// System call numbers
+#define SYS_WRITE 1
+#define SYS_READ  2
+#define SYS_EXIT  10
+#define SYS_SLEEP 11
+#define SYS_ALLOC 20
+#define SYS_FREE  21
+
 #endif // RUST_INTERFACE_H
